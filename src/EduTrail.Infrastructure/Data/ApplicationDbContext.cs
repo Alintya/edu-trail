21be17ca9042fc9f6eac968861bb6c1af3c6f42c
Alintya <Alintya@users.noreply.github.com>
2024-12-24 12:29:47 +0100

    refactor: make guid identity pk

--- src/EduTrail.Infrastructure/Data/ApplicationDbContext.cs
+++ src/EduTrail.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 using EduTrail.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EduTrail.Infrastructure.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>(options)
 {
 }
--- src/EduTrail.Infrastructure/Identity/ApplicationUser.cs
+++ src/EduTrail.Infrastructure/Identity/ApplicationUser.cs
@@ -2,6 +2,14 @@ using Microsoft.AspNetCore.Identity;
 
 namespace EduTrail.Infrastructure.Identity;
 
-public class ApplicationUser : IdentityUser
+public class ApplicationUser : IdentityUser<Guid>
 {
+    public bool IsValidUser()
+    {
+        // TODO: email regex
+        return !string.IsNullOrWhiteSpace(UserName)
+               && UserName.Length >= 3
+               && !string.IsNullOrWhiteSpace(Email)
+               && Email.Contains('@');
+    }
 }
