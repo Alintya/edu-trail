10959022a5cc625dd19fd712943b1c6624c7c678
Alintya <Alintya@users.noreply.github.com>
2024-12-27 19:29:12 +0100

    feat: add generic repository

---
+++ src/EduTrail.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,50 @@
+﻿using EduTrail.Domain.Interfaces;
+using EduTrail.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduTrail.Infrastructure.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly IDbContextFactory<ApplicationDbContext> contextFactory;
+
+    public Repository(IDbContextFactory<ApplicationDbContext> contextFactory)
+    {
+        this.contextFactory = contextFactory;
+    }
+
+    public virtual async Task<T?> GetByIdAsync(Guid id)
+    {
+        await using var context = await contextFactory.CreateDbContextAsync();
+        return await context.Set<T>().FindAsync(id);
+    }
+
+    public virtual async Task<IEnumerable<T>> GetAllAsync()
+    {
+        await using var context = await contextFactory.CreateDbContextAsync();
+        return await context.Set<T>().ToListAsync();
+    }
+
+    public virtual async Task<T> AddAsync(T entity)
+    {
+        await using var context = await contextFactory.CreateDbContextAsync();
+        await context.Set<T>().AddAsync(entity);
+        await context.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public virtual async Task UpdateAsync(T entity)
+    {
+        await using var context = await contextFactory.CreateDbContextAsync();
+        context.Set<T>().Update(entity);
+        await context.SaveChangesAsync();
+    }
+
+    public virtual async Task DeleteAsync(T entity)
+    {
+        await using var context = await contextFactory.CreateDbContextAsync();
+        context.Set<T>().Remove(entity);
+        await context.SaveChangesAsync();
+    }
+}
