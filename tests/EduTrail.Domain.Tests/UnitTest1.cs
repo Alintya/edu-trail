663bfc520ff4ce805921ff0e84bbe2b6a5aa6a51
Alintya <Alintya@users.noreply.github.com>
2025-01-08 11:02:41 +0100

    feat: add domain tests project

---
+++ tests/EduTrail.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace EduTrail.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+}
