dbab838f8f66a21c97aa1dff9a1bc46701b0fd10
Alintya <Alintya@users.noreply.github.com>
2025-01-15 03:02:46 +0100

    feat: add serilog

--- src/EduTrail.WebUI/Program.cs
+++ src/EduTrail.WebUI/Program.cs
@@ -4,66 +4,93 @@ using EduTrail.WebUI.Components;
 using EduTrail.WebUI.Components.Account;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+// The initial "bootstrap" logger is able to log errors during start-up. It's completely replaced by the
+// logger configured in `AddSerilog()` below, once configuration and dependency-injection have both been
+// set up successfully.
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Configuration.AddEnvironmentVariables("EDUTRAIL_");
-
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents()
-    .AddInteractiveWebAssemblyComponents();
-
-builder.Services.AddApplicationServices();
-builder.Services.AddInfrastructureLayer(builder.Configuration);
-
-// Configure Authentication
-builder.Services.AddAuthentication(options =>
+try
 {
-    options.DefaultScheme = IdentityConstants.ApplicationScheme;
-    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-})
-    .AddIdentityCookies();
+    Log.Information("Starting web application");
 
-builder.Services.AddCascadingAuthenticationState();
-builder.Services.AddScoped<IdentityUserAccessor>();
-builder.Services.AddScoped<IdentityRedirectManager>();
-builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+    builder.Configuration.AddEnvironmentVariables("EDUTRAIL_");
 
-var app = builder.Build();
+    builder.Host.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+        .ReadFrom.Configuration(hostingContext.Configuration)
+        .Enrich.FromLogContext()
+        .WriteTo.Console());
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
-    app.UseMigrationsEndPoint();
+    // Add services to the container.
+    builder.Services.AddRazorComponents()
+        .AddInteractiveServerComponents()
+        .AddInteractiveWebAssemblyComponents();
+
+    builder.Services.AddApplicationServices();
+    builder.Services.AddInfrastructureLayer(builder.Configuration);
+
+    // Configure Authentication
+    builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+    })
+        .AddIdentityCookies();
+
+    builder.Services.AddCascadingAuthenticationState();
+    builder.Services.AddScoped<IdentityUserAccessor>();
+    builder.Services.AddScoped<IdentityRedirectManager>();
+    builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
+
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+    await using var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseWebAssemblyDebugging();
+        app.UseMigrationsEndPoint();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Error", createScopeForErrors: true);
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseStaticFiles();
+    app.UseSerilogRequestLogging();
+    app.UseAntiforgery();
+
+    app.MapRazorComponents<App>()
+        .AddInteractiveServerRenderMode()
+        .AddInteractiveWebAssemblyRenderMode()
+        .AddAdditionalAssemblies(typeof(EduTrail.WebUI.Client._Imports).Assembly);
+
+    // Add additional endpoints required by the Identity /Account Razor components.
+    app.MapAdditionalIdentityEndpoints();
+
+    app.Lifetime.ApplicationStopping.Register(async () =>
+    {
+        // Perform cleanup here, e.g., flush logs, close connections, etc.
+        //app.Logger.LogInformation("Application is stopping...");
+    });
+
+    app.Run();
 }
-else
+catch (Exception ex)
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    Log.Fatal(ex, "Application terminated unexpectedly");
 }
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode()
-    .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(typeof(EduTrail.WebUI.Client._Imports).Assembly);
-
-// Add additional endpoints required by the Identity /Account Razor components.
-app.MapAdditionalIdentityEndpoints();
-
-app.Lifetime.ApplicationStopping.Register(async () =>
+finally
 {
-    // Perform cleanup here, e.g., flush logs, close connections, etc.
-    //app.Logger.LogInformation("Application is stopping...");
-});
-
-app.Run();
+    Log.CloseAndFlush();
+}
