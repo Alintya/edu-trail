9321218ce7e72cdefe6a1e8b734c68bd6dd727ce
Alintya <alintya@users.noreply.github.com>
2025-01-30 11:04:20 +0100

    chore: add Application test project

---
+++ tests/EduTrail.Application.IntegrationTests/IntegrationTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace EduTrail.Application.IntegrationTests;
+
+public class IntegrationTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+}
